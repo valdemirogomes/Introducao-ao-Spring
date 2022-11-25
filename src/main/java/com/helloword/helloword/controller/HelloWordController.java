97070ed0e22473078ba220a4fa8c4e1693aa2a9c
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2022-11-25 02:02:14 -0300

    Introducao ao Spring

--- src/main/java/com/helloword/helloword/controller/HelloWordController.java
+++ src/main/java/com/helloword/helloword/controller/HelloWordController.java
@@ -18,14 +18,26 @@ public class HelloWordController {
 
 	}
 
-	@GetMapping("/list")
+	@GetMapping("/list-bsm")
 	public List listBsm() {
 
 		List bsm = new ArrayList();
-		bsm.add("Java");
-		bsm.add("JS");
+		bsm.add("Orientação ao futuro");
+		bsm.add("Trabalho em equipe");
+		bsm.add("Orientação aos detalhes");
 
 		return bsm;
+
+	}
+	@GetMapping("/list-obj")
+	public List listObjetivos() {
+
+		List obj = new ArrayList();
+		obj.add("Spring");
+		obj.add("Arquitetura Rest");
+		obj.add("DER");
+
+		return obj;
 
 	}
 
